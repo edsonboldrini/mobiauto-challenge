26bd458c841716c76a30a3834bd5549b1f2d77af
Edson Boldrini <edsonboldrini@gmail.com>
2022-12-26 09:01:33 -0300

    Setup busca test

--- __tests__/busca.test.tsx
+++ __tests__/busca.test.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Busca from '../pages/busca'
+// import { useRouter } from 'next/router'
 import '@testing-library/jest-dom'
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn()
+}))
+
+// test('List renders with an empty query', () => {
+//   useRouter.mockReturnValue({ query: {}})
+//   render(<Busca brands={[]} />)
+//   const listItems = screen.getAllByRole('listitem')
+//   expect(listItems[0]).toHaveTextContent('Apple')
+//   expect(listItems[1]).toHaveTextContent('Banana')
+//   expect(listItems[2]).toHaveTextContent('Cherry')
+// })
 
 describe('Home', () => {
   it('renders a heading', () => {
-    render(<Busca brands={[]}/>)
+    render(<Busca brands={[]} />)
 
     const heading = screen.getByRole('heading', {
-      name: /Tabela Fipe/i,
+      name: /Tabela Fipe/i
     })
 
     expect(heading).toBeInTheDocument()
