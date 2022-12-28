6c29790e9fbb64ad66c2c096b0342a87f629348b
Edson Boldrini <edsonboldrini@gmail.com>
2022-12-28 16:29:18 -0300

    Update meta logo image

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -18,7 +18,7 @@ export default function DefaultLayout({ children, title = 'Mobiauto Challenge', 
         <title>{title}</title>
         <meta property="og:url" content={router?.asPath ?? 'https://mobiauto-challenge.vercel.app'} />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/logo.png" />
+        <meta property="og:image" content="/logo-white.png" />
         <meta property="og:title" content={metaTitle} />
         <meta property="og:description" content={metaDescription} />
       </Head>
